cb5b94b6ee67be92af92d4ba2d99bb22df15c1fa
DA.Morozov@urfu.me <DA.Morozov@urfu.me>
2021-06-03 12:02:15 +0500

    Добавлен полигон.

---
+++ WindowsFormsApp12/Polygon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp12
+{
+    public struct Polygon
+    {
+        public List<Vector> Vertexes { get; set; }
+        public Polygon(List<Vector> vertexes)
+        {
+            Vertexes = vertexes;
+        }
+    }
+}
